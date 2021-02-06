ac8194f3e65a8964b498e6f3572fe278a932129c
Karl Cheng <qantas94heavy@gmail.com>
2021-02-07 00:54:33 +1100

    Initial commit

---
+++ precache-manifest.9824d686a3f575e493a6b0d03236b190.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6339cdea4cf0ff1f6f5c99628777a2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6718dfa468edd8d6b3df",
+    "url": "/static/css/2.23267526.chunk.css"
+  },
+  {
+    "revision": "611f6e8bbeb51106e66e",
+    "url": "/static/css/main.fe60f6cd.chunk.css"
+  },
+  {
+    "revision": "6718dfa468edd8d6b3df",
+    "url": "/static/js/2.2a2ff2b4.chunk.js"
+  },
+  {
+    "revision": "eadc8ac8b6080df510389a008a76ce0a",
+    "url": "/static/js/2.2a2ff2b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "611f6e8bbeb51106e66e",
+    "url": "/static/js/main.3792390d.chunk.js"
+  },
+  {
+    "revision": "b2cdf7548912d26082a9",
+    "url": "/static/js/runtime-main.03e1a1e0.js"
+  },
+  {
+    "revision": "5081e581c3ac034e1304b0c0829e3940",
+    "url": "/static/media/2.5081e581.jpg"
+  },
+  {
+    "revision": "32a4a6c3bb7f7c9b90145e935fcaa50c",
+    "url": "/static/media/3.32a4a6c3.jpg"
+  },
+  {
+    "revision": "aed3fea1db31d79f4edf828b361aea99",
+    "url": "/static/media/aboutus-1.aed3fea1.jpg"
+  },
+  {
+    "revision": "4f5d0701bc96f8d3bf8e671004220e6e",
+    "url": "/static/media/aboutus-2.4f5d0701.jpg"
+  },
+  {
+    "revision": "eed1e659c146572450e9a5241e0dfd0c",
+    "url": "/static/media/bg.eed1e659.jpg"
+  },
+  {
+    "revision": "ce47420575491a4b1234adea60e38a3d",
+    "url": "/static/media/faq-1.ce474205.jpg"
+  },
+  {
+    "revision": "e94603caaaf68b303453b3f7ceb23703",
+    "url": "/static/media/mono-full-white-copy.e94603ca.png"
+  },
+  {
+    "revision": "623bee83daf0aaefc1e5c1ad78738128",
+    "url": "/static/media/past-shows-header.623bee83.jpg"
+  },
+  {
+    "revision": "966b6f898059b9523d702010db4acbc4",
+    "url": "/static/media/sponsor-2.966b6f89.png"
+  },
+  {
+    "revision": "419b0fea1acfbc676eda104afd4795ac",
+    "url": "/static/media/sponsor-3.419b0fea.png"
+  },
+  {
+    "revision": "30e252c74953d4979a5b5ca9a14a305d",
+    "url": "/static/media/sponsor-4.30e252c7.png"
+  },
+  {
+    "revision": "0274ae9421f1244c27414b4d949b64d9",
+    "url": "/static/media/team-cast.0274ae94.jpg"
+  },
+  {
+    "revision": "8088feb0247e0cb29744229135c68f6a",
+    "url": "/static/media/team-marketing.8088feb0.jpg"
+  },
+  {
+    "revision": "02c463f4f25c2f6929c9a7f7b9a5ad0d",
+    "url": "/static/media/team-tech.02c463f4.jpg"
+  },
+  {
+    "revision": "1e24b0c17a404d0979c571559c4062b7",
+    "url": "/static/media/teams.1e24b0c1.jpg"
+  }
+]);
